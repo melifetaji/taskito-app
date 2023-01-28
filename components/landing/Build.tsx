ac9ac49012533d65c6a9c86f9a0bba49fb3785c3
Meli Fetaji <melfetaji@gmail.com>
2023-01-28 23:35:12 +0100

    Fixed Utils Button

--- components/landing/Build.tsx
+++ components/landing/Build.tsx
@@ -6,7 +6,7 @@ type Props = {};
 const Build = (props: Props) => {
   return (
     <div className="bg-[url('/BG.svg')] bg-no-repeat bg-cover min-h-[35em] flex justify-center items-center">
-      <div className="container mx-auto flex-col p-4 md:max-w-[55%]">
+      <div className="container mx-auto flex-col p-4 md:max-w-[55%] flex justify-center items-center">
         <h1 className="text-zinc-100 text-4xl md:text-7xl font-extrabold text-center mb-3">
           Achieve your goals and stay on track
         </h1>
@@ -14,7 +14,10 @@ const Build = (props: Props) => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores nemo
           doloremque nam commodi reiciendis. Accusantium?
         </p>
-        <Button className="bg-black px-6 py-2 hover:bg-gradient-to-r from-violet-600 to-orange-400 box-border border-2 border-transparent hover:border-violet-700 ">
+        <Button
+          className="px-6 py-2 hover:bg-gradient-to-r from-violet-600 to-orange-400 box-border border-2 border-transparent hover:border-violet-700"
+          type="black"
+        >
           Get started for free
         </Button>
       </div>
--- components/utils/Button.tsx
+++ components/utils/Button.tsx
@@ -3,13 +3,19 @@ import React from "react";
 type Props = {
   className?: string;
   children: React.ReactNode;
-  type?: "primary" | "secondary";
+  type?: "primary" | "secondary" | "black";
   size?: "large" | "medium";
 };
 
 const Button = ({ children, className, type, size }: Props) => {
-  let background = type === "primary" ? "bg-blue-600" : "bg-gray-600";
-  let btnSize = size === "large" ? "px-8 py-4 text-xl" : "";
+  let background = "bg-black";
+  if (type === "primary") {
+    let background = "bg-blue-600";
+  } else if (type === "secondary") {
+    let background = "bg-gray-600";
+  }
+
+  const btnSize = size === "large" ? "px-8 py-4 text-xl" : "";
   return (
     <div
       className={`cursor-pointer max-w-fit inline-block px-4 py-2 ${background} hover:bg-blue-700 rounded-lg text-white ${btnSize} ${className}`}
